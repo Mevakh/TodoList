dba4fbd8e6b10b2280beda2a28e24afdb5b029ab
Mevakh <107620982+Mevakh@users.noreply.github.com>
2023-03-21 17:39:07 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+let task = $("<div class='todoList'></div>")
+let del = $("<i class='fa-sharp fa-solid fa-pen-to-square'></i>")
+const edit = $("<i class='fa-sharp fa-solid fa-trash'></i>")
+
+
+$(".todoInput").on("keyup",function(e){
+    if(e.keyCode== 13 && $(".todoInput").val() !=""){
+        let task = $("<div class='todoList'></div>").text($(".todoInput").val());
+        
+        let del = $("<div class='buttons'><i class='fa-sharp fa-solid fa-trash'></i></div>")
+        .click(function(){
+            let p = $(this).parent();
+            p.remove() 
+        })
+        
+        task.append(del)
+        $(".todoLists").append(task)
+    }
+})
+
+
+
